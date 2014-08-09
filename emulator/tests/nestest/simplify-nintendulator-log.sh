b6b53f3f3d4d4cb1830fad21c675e022a76d68d9
Jan Pikl <pikl.jan@gmail.com>
2014-08-09 20:23:03 +0200

    Fixed nestest log generator script.

--- emulator/tests/nestest/simplify-nintendulator-log.sh
+++ emulator/tests/nestest/simplify-nintendulator-log.sh
@@ -4,4 +4,4 @@ readonly INPUT_FILE=./nintendulator-full.log
 readonly OUTPUT_FILE=./nintendulator-simple.log
 readonly LINES_COUNT=8991
 
-sed "s/^\(.\{20\}\).\{27\}\(.\{26\}\).*$$/\1\2/g" ${INPUT_FILE} | sed "s/\*/ /g" | head -n ${LINES_COUNT} > ${OUTPUT_FILE}
+sed "s/^\(.\{20\}\).\{27\}\(.\{26\}\).*$/\1\2/g" ${INPUT_FILE} | sed "s/\*/ /g" | head -n ${LINES_COUNT} > ${OUTPUT_FILE}
