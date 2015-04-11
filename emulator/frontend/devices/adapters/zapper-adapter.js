93270bb43db253ece43b00348407bbb18e4e3cc5
jpikl <pikl.jan@gmail.com>
2015-04-11 23:15:58 +0200

    Refactored zapper adapter

--- emulator/frontend/devices/adapters/zapper-adapter.js
+++ emulator/frontend/devices/adapters/zapper-adapter.js
@@ -1,31 +1,38 @@
 import { VIDEO_WIDTH, VIDEO_HEIGHT } from "../../../core/common/constants";
 
-export function ZapperAdapter(zapper) {
-  this.zapper = zapper;
+//=========================================================
+// Adapter for zapper device
+//=========================================================
+
+export class ZapperAdapter {
+
+    constructor(zapper) {
+        this.zapper = zapper;
+    }
+
+    init(videoManager) {
+        this.videoManager = videoManager;
+    }
+
+    getDevice() {
+        return this.zapper;
+    }
+
+    inputChanged(input, down) {
+        if (input === "trigger") {
+            this.zapper.setTriggerPressed(down);
+        }
+    }
+
+    stateChanged(state) {
+        var rect = this.videoManager.getOutputRect();
+        var horizontalScale = (rect.right - rect.left) / VIDEO_WIDTH;
+        var verticalScale = (rect.bottom - rect.top) / VIDEO_HEIGHT;
+        var x = ~~(((state.cursorX || 0) - rect.left) / horizontalScale);
+        var y = ~~(((state.cursorY || 0) - rect.top) / verticalScale);
+        this.zapper.setBeamPosition(x, y);
+    }
+
 }
 
 ZapperAdapter["dependencies"] = [ "videoManager" ];
-
-ZapperAdapter.prototype.init = function(videoManager) {
-  return this.videoManager = videoManager;
-};
-
-ZapperAdapter.prototype.getDevice = function() {
-  return this.zapper;
-};
-
-ZapperAdapter.prototype.inputChanged = function(input, down) {
-  if (input === "trigger") {
-    return this.zapper.setTriggerPressed(down);
-  }
-};
-
-ZapperAdapter.prototype.stateChanged = function(state) {
-  var horizontalScale, rect, verticalScale, x, y;
-  rect = this.videoManager.getOutputRect();
-  horizontalScale = (rect.right - rect.left) / VIDEO_WIDTH;
-  verticalScale = (rect.bottom - rect.top) / VIDEO_HEIGHT;
-  x = ~~(((state.cursorX || 0) - rect.left) / horizontalScale);
-  y = ~~(((state.cursorY || 0) - rect.top) / verticalScale);
-  return this.zapper.setBeamPosition(x, y);
-};
