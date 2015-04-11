eec4be9b66b4e0bb2de698ca2c46a18060e8a05a
jpikl <pikl.jan@gmail.com>
2015-04-11 23:09:40 +0200

    Refactored joypad adapter

--- emulator/frontend/devices/adapters/joypad-adapter.js
+++ emulator/frontend/devices/adapters/joypad-adapter.js
@@ -1,28 +1,35 @@
 import { Joypad, Button } from "../../../core/devices/joypad";
 
 var buttonAliases = {
-  "a": Button.A,
-  "b": Button.B,
-  "select": Button.SELECT,
-  "start": Button.START,
-  "up": Button.UP,
-  "down": Button.DOWN,
-  "left": Button.LEFT,
-  "right": Button.RIGHT
+    "a":      Button.A,
+    "b":      Button.B,
+    "select": Button.SELECT,
+    "start":  Button.START,
+    "up":     Button.UP,
+    "down":   Button.DOWN,
+    "left":   Button.LEFT,
+    "right":  Button.RIGHT
 };
 
-export function JoypadAdapter(joypad) {
-  this.joypad = joypad;
+//=========================================================
+// Adapter for joypad device
+//=========================================================
+
+export class JoypadAdapter {
+
+    constructor(joypad) {
+        this.joypad = joypad;
+    }
+
+    getDevice() {
+        return this.joypad;
+    }
+
+    inputChanged(input, down) {
+        var button = buttonAliases[input];
+        if (button != null) {
+            this.joypad.setButtonPressed(button, down);
+        }
+    }
+
 }
-
-JoypadAdapter.prototype.getDevice = function() {
-  return this.joypad;
-};
-
-JoypadAdapter.prototype.inputChanged = function(input, down) {
-  var button;
-  button = buttonAliases[input];
-  if (button != null) {
-    return this.joypad.setButtonPressed(button, down);
-  }
-};
