8dd34f737458127d6a5b2180e63459ec7f350988
jpikl <pikl.jan@gmail.com>
2015-04-12 14:48:00 +0200

    Refactored DebugPPU.

--- emulator/core/debug/debug-ppu.js
+++ emulator/core/debug/debug-ppu.js
@@ -1,22 +1,18 @@
 import { PPU } from "../units/ppu";
 
-  var
-    extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
-    hasProp = {}.hasOwnProperty;
+//=========================================================
+// PPU with disabled output generation
+//=========================================================
 
+export class DebugPPU extends PPU {
 
-export function DebugPPU() {
-  return DebugPPU.__super__.constructor.apply(this, arguments);
+    updateRGBAPalette() {
+    }
+
+    setFramePixel() {
+    }
+
+    clearFramePixel() {
+    }
+
 }
-
-extend(DebugPPU, PPU);
-
-DebugPPU.prototype.updateRGBAPalette = function() {
-};
-
-DebugPPU.prototype.setFramePixel = function() {
-};
-
-DebugPPU.prototype.clearFramePixel = function() {
-};
-
