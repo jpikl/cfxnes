7ae777ae2b0825f956c163ceb85364bbce1475bf
jpikl <pikl.jan@gmail.com>
2015-04-18 19:07:30 +0200

    Refactored audio manager.

--- emulator/frontend/managers/audio-manager.js
+++ emulator/frontend/managers/audio-manager.js
@@ -2,156 +2,164 @@ import { arrayToProperties } from "../../core/utils/arrays";
 import { logger }            from "../../core/utils/logger";
 import { forEeachProperty }  from "../../core/utils/objects";
 
-var
-  bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
-
-const BUFFER_SIZE = 4096;
-const DEFAULT_ENABLED = true;
-const DEFAULT_VOLUME = 1.0;
-
 const channelAliases = {
-  "pulse1": 0,
-  "pulse2": 1,
-  "triangle": 2,
-  "noise": 3,
-  "dmc": 4
+    "pulse1":   0,
+    "pulse2":   1,
+    "triangle": 2,
+    "noise":    3,
+    "dmc":      4
 };
 
 export const channels = Object.keys(channelAliases);
 
-export function AudioManager() {
-  this.updateAudio = bind(this.updateAudio, this);
+//=========================================================
+// Audio manager
+//=========================================================
+
+export class AudioManager {
+
+    init(nes) {
+        logger.info("Initializing audio manager");
+        this.nes = nes;
+        if (this.isSupported()) {
+            this.createAudio();
+        }
+        this.setDefaults();
+    }
+
+    setDefaults() {
+        logger.info("Using default audio configuration");
+        this.setEnabled();
+        this.setVolume();
+        for (channel of channels) {
+            this.setChannelEnabled(channel);
+        }
+    }
+
+    //=========================================================
+    // Audio context
+    //=========================================================
+
+    isSupported() {
+        return typeof AudioContext !== "undefined" && AudioContext !== null;
+    }
+
+    createAudio() {
+        logger.info("Creating audio context");
+        this.context = new AudioContext;
+        this.processor = this.context.createScriptProcessor(4096, 0, 1); // 4K buffer, 0 input channels, 1 output channel
+        this.processor.onaudioprocess = this.updateAudio.bind(this);
+        this.nes.initAudioRecording(this.processor.bufferSize, this.context.sampleRate);
+    }
+
+    updateAudio(event) {
+        var outputBuffer = event.outputBuffer;
+        var sourceBuffer = this.nes.readAudioBuffer();
+        var outputChannel = outputBuffer.getChannelData(0);
+        for (var i = 0; i < outputChannel.length; i++) {
+            outputChannel[i] = this.volume * sourceBuffer[i];
+        }
+    }
+
+    //=========================================================
+    // Audio state
+    //=========================================================
+
+    setEnabled(enabled = true) {
+        if (this.enabled !== enabled) {
+            logger.info(`Audio ${enabled ? 'on' : 'off'}`);
+            this.enabled = enabled;
+            this.updateState();
+        }
+    }
+
+    isEnabled() {
+        return this.enabled;
+    }
+
+    setPlaying(playing) {
+        if (this.playing !== playing) {
+            logger.info(`Audio ${playing ? 'resumed' : 'paused'}`);
+            this.playing = playing;
+            this.updateState();
+        }
+    }
+
+    setSpeed(speed) {
+        if (this.speed !== speed) {
+            logger.info(`Setting audio recording speed to ${speed}x`);
+            this.speed = speed;
+            this.updateState();
+        }
+    }
+
+    updateState() {
+        if (this.isSupported()) {
+            if (this.enabled && this.playing) {
+                this.nes.startAudioRecording(this.context.sampleRate / this.speed);
+                this.processor.connect(this.context.destination);
+            } else {
+                this.nes.stopAudioRecording();
+                this.processor.disconnect();
+            }
+        }
+    }
+
+    //=========================================================
+    // Audio channels
+    //=========================================================
+
+    setChannelEnabled(channel, enabled = true) {
+        var channelId = channelAliases[channel]
+        if (channelId != null && this.isChannelEnabled(channel) !== enabled) {
+            logger.info(`Audio channel '${channel}' ${enabled ? 'on' : 'off'}`);
+            this.nes.setChannelEnabled(channelId, enabled);
+        }
+    }
+
+    isChannelEnabled(channel) {
+        return this.nes.isChannelEnabled(channelAliases[channel]);
+    }
+
+    //=========================================================
+    // Audio volume
+    //=========================================================
+
+    setVolume(volume = 1.0) {
+        if (this.volume != volume) {
+            this.volume = Math.max(0.0, Math.min(volume, 1.0));
+            logger.info(`Setting audio volume to ${~~(100 * this.volume)}%`);
+        }
+    }
+
+    getVolume() {
+        return this.volume;
+    }
+
+    //=========================================================
+    // Configuration reading / writing
+    //=========================================================
+
+    readConfiguration() {
+        logger.info("Reading audio manager configuration");
+        return {
+            "enabled":  this.isEnabled(),
+            "volume":   this.getVolume(),
+            "channels": arrayToProperties(channels, this.isChannelEnabled, this)
+        }
+    }
+
+    writeConfiguration(config) {
+        if (config) {
+            logger.info("Writing audio manager configuration");
+            this.setEnabled(config["enabled"]);
+            this.setVolume(config["volume"]);
+            if (config["channels"]) {
+                forEeachProperty(config["channels"], this.setChannelEnabled, this);
+            }
+        }
+    }
+
 }
 
 AudioManager["dependencies"] = [ "nes" ];
-
-AudioManager.prototype.init = function(nes) {
-  var channel;
-  logger.info("Initializing audio manager");
-  this.nes = nes;
-  if (this.isSupported()) {
-    this.createAudio();
-  }
-  return this.setDefaults();
-};
-
-AudioManager.prototype.setDefaults = function() {
-  var channel, j, len, ref, results;
-  logger.info("Using default audio configuration");
-  this.setEnabled(DEFAULT_ENABLED);
-  this.setVolume(DEFAULT_VOLUME);
-  ref = channels;
-  results = [];
-  for (j = 0, len = ref.length; j < len; j++) {
-    channel = ref[j];
-    results.push(this.setChannelEnabled(channel, DEFAULT_ENABLED));
-  }
-  return results;
-};
-
-AudioManager.prototype.isSupported = function() {
-  return typeof AudioContext !== "undefined" && AudioContext !== null;
-};
-
-AudioManager.prototype.createAudio = function() {
-  logger.info("Creating audio context");
-  this.context = new AudioContext;
-  this.processor = this.context.createScriptProcessor(BUFFER_SIZE, 0, 1);
-  this.processor.onaudioprocess = this.updateAudio;
-  return this.nes.initAudioRecording(BUFFER_SIZE, this.context.sampleRate);
-};
-
-AudioManager.prototype.updateAudio = function(event) {
-  var i, j, outputBuffer, outputChannel, ref, sourceBuffer;
-  outputBuffer = event.outputBuffer;
-  sourceBuffer = this.nes.readAudioBuffer();
-  outputChannel = outputBuffer.getChannelData(0);
-  for (i = j = 0, ref = BUFFER_SIZE; 0 <= ref ? j < ref : j > ref; i = 0 <= ref ? ++j : --j) {
-    outputChannel[i] = this.volume * sourceBuffer[i];
-  }
-  return void 0;
-};
-
-AudioManager.prototype.setEnabled = function(enabled) {
-  if (enabled == null) {
-    enabled = DEFAULT_ENABLED;
-  }
-  logger.info("Audio " + (enabled ? 'on' : 'off'));
-  this.enabled = enabled;
-  return this.updateState();
-};
-
-AudioManager.prototype.isEnabled = function() {
-  return this.enabled;
-};
-
-AudioManager.prototype.setPlaying = function(playing) {
-  logger.info("Audio " + (playing ? 'resumed' : 'paused'));
-  this.playing = playing;
-  return this.updateState();
-};
-
-AudioManager.prototype.setSpeed = function(speed) {
-  logger.info("Setting audio recording speed to " + speed + "x");
-  this.speed = speed;
-  return this.updateState();
-};
-
-AudioManager.prototype.updateState = function() {
-  if (!this.isSupported()) {
-    return;
-  }
-  if (this.enabled && this.playing) {
-    this.nes.startAudioRecording(this.context.sampleRate / this.speed);
-    return this.processor.connect(this.context.destination);
-  } else {
-    this.nes.stopAudioRecording();
-    return this.processor.disconnect();
-  }
-};
-
-AudioManager.prototype.setChannelEnabled = function(channel, enabled) {
-  if (enabled == null) {
-    enabled = DEFAULT_ENABLED;
-  }
-  if (channelAliases[channel] != null) {
-    logger.info("Audio channel '" + channel + "' " + (enabled ? 'on' : 'off'));
-    return this.nes.setChannelEnabled(channelAliases[channel], enabled);
-  }
-};
-
-AudioManager.prototype.isChannelEnabled = function(channel) {
-  return this.nes.isChannelEnabled(channelAliases[channel]);
-};
-
-AudioManager.prototype.setVolume = function(volume) {
-  if (volume == null) {
-    volume = DEFAULT_VOLUME;
-  }
-  this.volume = Math.max(0.0, Math.min(volume, 1.0));
-  return logger.info("Setting audio volume to " + (~~(100 * this.volume)) + "%");
-};
-
-AudioManager.prototype.getVolume = function() {
-  return this.volume;
-};
-
-AudioManager.prototype.readConfiguration = function() {
-    logger.info("Reading audio manager configuration");
-    return {
-        "enabled":  this.isEnabled(),
-        "volume":   this.getVolume(),
-        "channels": arrayToProperties(channels, this.isChannelEnabled, this)
-    };
-};
-
-AudioManager.prototype.writeConfiguration = function(config) {
-    if (config) {
-        logger.info("Writing audio manager configuration");
-        this.setEnabled(config["enabled"]);
-        this.setVolume(config["volume"]);
-        if (config["channels"]) {
-            forEeachProperty(config["channels"], this.setChannelEnabled, this);
-        }
-    }
-};
