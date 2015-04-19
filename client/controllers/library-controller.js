49fa87d686bf10c5d066ca7e84d08b6b9a978d7f
jpikl <pikl.jan@gmail.com>
2015-04-19 22:51:40 +0200

    Refactored library controller.

---
+++ client/controllers/library-controller.js
@@ -0,0 +1,18 @@
+angular.module("cfxnes").controller("LibraryController", ($scope, $state, library, emulator, globalParams) => {
+
+    $scope.gameFilter = globalParams.gameFilter;
+    $scope.loading = true;
+
+    $scope.playGame = (game) => {
+        $state.go("emulator", { gameId: game.id });
+    };
+
+    library.getROMs().success((data) => {
+        $scope.games = data;
+        $scope.loading = false;
+    }).error((data, status) => {
+        $scope.error = `Unable to download game list (server response: ${status})`;
+        $scope.loading = false;
+    });
+
+});
