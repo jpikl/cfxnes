2aa58057a52191ce171c8a03eaecb77d45b3d389
jpikl <pikl.jan@gmail.com>
2015-04-23 01:15:57 +0200

    Gulpfile rewrite.

--- gulpfile.js
+++ gulpfile.js
@@ -3,32 +3,50 @@ var gulp    = require("gulp");
 var babel   = require("gulp-babel");
 var closure = require("gulp-closurecompiler");
 var concat  = require("gulp-concat");
+var gulpif  = require("gulp-if");
 var jade    = require("gulp-jade");
 var mocha   = require("gulp-mocha");
 var rename  = require("gulp-rename");
 var stylus  = require("gulp-stylus");
+var uglify  = require("gulp-uglify");
 var util    = require("gulp-util");
+var mkdirp  = require("mkdirp");
 
-var devMode = util.env.dev === true;
-var proMode = !devMode;
+//=========================================================
+// Mode helpers
+//=========================================================
 
-function modeMin(name) {
-    return proMode ? name.replace(".js", ".min.js")
-                         .replace(".css", ".min.css")
-                   : name;
+var mode = "production";
+
+function production() {
+    return mode === "production";
 }
 
-function modeChoice(proChoice, devChoice) {
-    return proMode ? proChoice : devChoice;
+function development() {
+    return mode === "development";
 }
+
+function mchoice(productionValue, developmentValue) {
+    return production() ? productionValue : developmentValue;
+}
+
+function minname(developmentName) {
+    var productionName = developmentName.replace(".js", ".min.js")
+                                        .replace(".css", ".min.css");
+    return mchoice(productionName, developmentName);
+}
+
+//=========================================================
+// Library
+//=========================================================
 
 gulp.task("lib", function() {
     return gulp.src(["./src/lib/**/*.js", "!./src/lib/core/debug/**"])
         .pipe(closure({
-                fileName: modeMin("cfxnes.js")
+                fileName: minname("cfxnes.js")
             }, {
-                create_source_map: "./dist/lib/" + modeMin("cfxnes.js.map"),
-                compilation_level: modeChoice("ADVANCED", "SIMPLE"),
+                create_source_map: "./dist/lib/" + minname("cfxnes.js.map"),
+                compilation_level: mchoice("ADVANCED", "SIMPLE"),
                 language_in: "ECMASCRIPT6",
                 language_out: "ES5",
                 externs: [
@@ -37,50 +55,99 @@ gulp.task("lib", function() {
                     "./externs/w3c_audio.js"
                 ]
             }))
-        .pipe(gulp.dest("./dist/lib/"));
-});
-
-gulp.task("client-scripts", function() {
-    return gulp.src("./src/app/client/**/*.js")
-        .pipe(babel())
-        .pipe(concat(modeMin("app.js")))
+        .pipe(gulp.dest("./dist/lib/"))
         .pipe(gulp.dest("./dist/app/static/scripts/"));
 });
 
-gulp.task("client-styles", function() {
+//=========================================================
+// Client resources
+//=========================================================
+
+gulp.task("scripts", function() {
+    return gulp.src("./src/app/client/**/*.js")
+        .pipe(babel())
+        .pipe(gulpif(production(), uglify()))
+        .pipe(concat(minname("app.js")))
+        .pipe(gulp.dest("./dist/app/static/scripts/"));
+});
+
+gulp.task("styles", function() {
     return gulp.src("./src/app/client/**/*.styl")
         .pipe(stylus())
-        .pipe(concat(modeMin("app.css")))
+        .pipe(concat(minname("app.css")))
         .pipe(gulp.dest("./dist/app/static/styles/"));
 });
 
-gulp.task("client-views", function() {
+gulp.task("views", function() {
     return gulp.src("./src/app/client/**/*.jade")
         .pipe(jade({
-            pretty: devMode,
-            compileDebug: devMode,
-            data: {devMode: devMode, proMode: proMode}
+            pretty: development(),
+            compileDebug: development(),
+            data: {mode: mode}
         }))
         .pipe(gulp.dest("./dist/app/static/"));
 });
 
-gulp.task("client-images", function() {
+gulp.task("images", function() {
     return gulp.src("./src/app/client/**/*.{png,jpg,gif}")
         .pipe(gulp.dest("./dist/app/static/"));
 });
 
-gulp.task("client-lib", function() {
-    return gulp.src([modeMin("./dist/lib/cfxnes.js"), modeMin("./dist/lib/cfxnes.js.map")])
-        .pipe(gulp.dest("./dist/app/static/scripts/"));
+//=========================================================
+// Third party resources
+//=========================================================
+
+gulp.task("vendor-scripts", function() {
+    return gulp.src([
+                mchoice("./bower_components/seiyria-bootstrap-slider/dist/bootstrap-slider.min.js",
+                        "./bower_components/seiyria-bootstrap-slider/js/bootstrap-slider.js"), // Different .min file path
+                minname("./bower_components/jquery/dist/jquery.js"),
+                minname("./bower_components/angular/angular.js"),
+                minname("./bower_components/angular-ui-router/release/angular-ui-router.js"),
+                minname("./bower_components/angular-bootstrap/ui-bootstrap-tpls.js"),
+                minname("./bower_components/js-md5/js/md5.js"),
+                "./bower_components/angular-bootstrap-slider/slider.js", // Missing .min file
+                "./bower_components/screenfull/dist/screenfull.js" // Missing .min file
+            ])
+        .pipe(gulp.dest("./dist/app/static/scripts/"))
+});
+
+gulp.task("vendor-styles", function() {
+    return gulp.src([
+                minname("./bower_components/bootstrap/dist/css/bootstrap.css"),
+                minname("./bower_components/bootstrap/dist/css/bootstrap-theme.css"),
+                minname("./bower_components/seiyria-bootstrap-slider/dist/css/bootstrap-slider.css")
+            ])
+        .pipe(gulp.dest("./dist/app/static/styles/"))
+});
+
+gulp.task("vendor-fonts", function() {
+    return gulp.src("./bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.*")
+        .pipe(gulp.dest("./dist/app/static/fonts/"))
+});
+
+//=========================================================
+// Application
+//=========================================================
+
+gulp.task("init", function(done) {
+    mkdirp("./dist/", done);
 });
 
 gulp.task("app", gulp.parallel(
-    "client-scripts",
-    "client-styles",
-    "client-views",
-    "client-images",
-    gulp.series("lib", "client-lib")
+    "lib",
+    "scripts",
+    "styles",
+    "views",
+    "images",
+    "vendor-scripts",
+    "vendor-styles",
+    "vendor-fonts"
 ));
+
+//=========================================================
+// Tests
+//=========================================================
 
 gulp.task("test", function() {
     require("babel/register");
@@ -88,8 +155,23 @@ gulp.task("test", function() {
         .pipe(mocha({ timeout: 60000 }));
 });
 
+//=========================================================
+// Development
+//=========================================================
+
+gulp.task("set-dev-mode", function(done) {
+    mode = "development";
+    done();
+})
+
+gulp.task("dev", gulp.series("set-dev-mode", "init", "app"))
+
+//=========================================================
+// Other
+//=========================================================
+
 gulp.task("clean", function(done) {
     del(["./dist/"], done);
 });
 
-gulp.task("default", gulp.series("app"));
+gulp.task("default", gulp.series("init", "app"));
